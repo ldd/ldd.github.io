443c0a5555b59d44d2025bdd3c8d9e25c9aff7a9
ldd <leonardoldd@gmail.com>
2015-02-10 23:13:50 -0500

    tileset update

--- demo/data/map/tileset.tsx
+++ demo/data/map/tileset.tsx
@@ -1,28 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tileset" tilewidth="35" tileheight="35">
  <image source="../img/tileset.png" width="512" height="512"/>
- <tile id="28">
+ <tile id="58">
   <animation>
-   <frame tileid="28" duration="400"/>
-   <frame tileid="29" duration="400"/>
+   <frame tileid="58" duration="400"/>
+   <frame tileid="59" duration="400"/>
   </animation>
  </tile>
- <tile id="29">
+ <tile id="59">
   <animation>
-   <frame tileid="29" duration="400"/>
-   <frame tileid="28" duration="400"/>
+   <frame tileid="59" duration="400"/>
+   <frame tileid="58" duration="400"/>
   </animation>
  </tile>
- <tile id="42">
+ <tile id="87">
   <animation>
-   <frame tileid="42" duration="400"/>
-   <frame tileid="43" duration="400"/>
+   <frame tileid="87" duration="400"/>
+   <frame tileid="88" duration="400"/>
   </animation>
  </tile>
- <tile id="43">
+ <tile id="88">
   <animation>
-   <frame tileid="43" duration="400"/>
-   <frame tileid="42" duration="400"/>
+   <frame tileid="88" duration="400"/>
+   <frame tileid="87" duration="400"/>
   </animation>
  </tile>
 </tileset>
