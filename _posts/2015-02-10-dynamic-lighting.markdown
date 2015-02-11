a318700abd6c3f64f73cc1a4239709adc0d133ad
ldd <leonardoldd@gmail.com>
2015-02-10 21:33:52 -0500

    demo test

---
+++ _posts/2015-02-10-dynamic-lighting.markdown
@@ -0,0 +1,9 @@
+---
+layout:     simple
+title:      "Extra"
+subtitle:   "Extra"
+date:       2015-02-06 12:00:00
+author:     "Ldd"
+---
+<!-- Canvas placeholder -->
+<div id="screen"></div>
---
+++ demo/js/resources.js
@@ -0,0 +1,55 @@
+game.resources = [
+
+    /* Graphics.
+     * @example
+     * {name: "example", type:"image", src: "../data/img/example.png"},
+     */
+    {name: "tileset",         type:"image",   src: "../../../../demo/data/img/tileset.png"},
+    {name: "atascii",         type:"image",   src: "../../../../demo/data/img/atascii_24px.png"},
+    {name: "background",      type:"image",   src: "../../../../demo/data/img/background.png"},
+
+
+    /* Maps.
+     * @example
+     * {name: "example01", type: "tmx", src: "demo/data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "demo/data/map/example01.json"},
+     */
+    {name: "map1",            type: "tmx",    src: "../../../../demo/data/map/map1.json"},
+    {name: "map2",            type: "tmx",    src: "../../../../demo/data/map/map2.tmx"},
+
+
+    /* Tilesets.
+     * @example
+     * {name: "example01", type: "tsx", src: "../data/map/example01.tsx"},
+     */
+    {name: "tileset",         type: "tsx",    src: "../../../../demo/data/map/tileset.tsx"},
+
+
+    /* Background music.
+     * @example
+     * {name: "example_bgm", type: "audio", src: "../data/bgm/" },
+     */
+    {name: "DST-GameForest",  type: "audio",  src: "../../../../demo/data/bgm/" },
+
+    /* Sound effects.
+     * @example
+     * {name: "example_sfx", type: "audio", src: "../data/sfx/"}
+     */
+    {name: "cling",           type: "audio",  src: "../../../../demo/data/sfx/"},
+    {name: "die",             type: "audio",  src: "../../../../demo/data/sfx/"},
+    {name: "enemykill",       type: "audio",  src: "../../../../demo/data/sfx/"},
+    {name: "jump",            type: "audio",  src: "../../../../demo/data/sfx/"},
+
+
+    /* Atlases
+     * @example
+     * {name: "example_tps", type: "json", src: "../data/img/example_tps.json"},
+     */
+    // texturePacker
+    {name: "texture",         type: "json",   src: "../../../../demo/data/img/texture.json"},
+    {name: "texture",         type: "image",  src: "../../../../demo/data/img/texture.png"}
+    // ShoeBox
+    //{name: "texture",         type: "json",   src: "../data/gfx/shoebox.json"},
+    //{name: "texture",         type: "image",  src: "../data/gfx/shoebox.png"}
+
+];
